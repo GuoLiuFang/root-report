6942327538d60443bd38b7d0c8cbd61507a0f0d5
LiuFangGuo <guoliufang@tigerjoys.com>
2015-10-16 12:23:57 +0800

    修改生成grep脚本命令的错误

--- src/com/tj/services/ErrorAddressService.java
+++ src/com/tj/services/ErrorAddressService.java
@@ -65,8 +65,12 @@ public class ErrorAddressService {
 
     private String getExeContent(String... args) {
         String date_time = args[1];
-        String contentFileName = "bingo." + date_time + ".log";
-        return this.properties.getProperty("exeWay") + " " + this.properties.getProperty("shellPath") + " " + this.properties.getProperty("contentPath") + contentFileName + " " + args[0] + " " + args[1];
+        String[] dateAndTime = date_time.split("[ ]");
+        String contentFileName = "bingo." + dateAndTime[0] + ".log";
+        date_time = "'" + date_time + "'";
+        String exeContent = this.properties.getProperty("exeWay") + " " + this.properties.getProperty("shellPath") + " " + this.properties.getProperty("contentPath") + contentFileName + " " + args[0] + " " + date_time;
+        System.out.println(exeContent);
+        return exeContent;
     }
 
 
@@ -78,7 +82,8 @@ public class ErrorAddressService {
 //            System.out.println("日期是：" + errorAddress.getDid());
 //        }
         ErrorAddressService errorAddressService = ErrorAddressService.getErrorAddressService();
-        String result = errorAddressService.getDetails("4beaec8b-ec1b-4ff8-844d-5e5ddd579020", "2015-09-29 08:49:55");
+//        String result = errorAddressService.getDetails("4beaec8b-ec1b-4ff8-844d-5e5ddd579020", "2015-09-29 08:49:55");
+        String result = errorAddressService.getExeContent("76339d39-3c02-40d3-b617-68afaaeb2eb0", "2015-10-07 14:04:29");
         System.out.println("-------执行结果是-------" + result);
     }
 
